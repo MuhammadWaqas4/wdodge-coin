389ab2bd7346b8fb6afb72dada13226f383e6d47
FIRST_NAME LAST_NAME <mwaqasbashir4@gmail.com>
2021-08-09 23:04:46 +0500

    q

--- src/Components/wdodge.js
+++ src/Components/wdodge.js
@@ -184,6 +184,7 @@ function Wdodge() {
             console.log("error", e.mesage);
         }
     };
+
     const onchangeinput = async (e) => {
         try {
 
@@ -282,7 +283,7 @@ function Wdodge() {
                         <div className="container mx-auto relative z-30 text-white px-4 lg:px-32">
                             <nav className="py-8 mb-8 lg:mb-16 flex items-center justify-between">
                                 <div className="flex items-center"><img src="logo.png" alt="" className="w-12 h-12" />
-                                    <div className="font-bold text-3xl ml-2">wDogecoin</div>
+                                    <div className="font-bold text-3xl ml-2">PontusChain</div>
                                 </div>
                                 <div className="hidden flex-1 lg:flex lg:space-x-4 flex-wrap justify-end"><a href="#about"
                                     className="text-right w-full lg:w-auto text-xl font-medium hover:text-doge-s">About</a><a
@@ -320,7 +321,7 @@ function Wdodge() {
                                         d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z">
                                     </path>
                                 </svg></a></div>
-                                <div className=""><a href="https://t.me/wDogecoin" className="" target="_blank"><svg
+                                <div className=""><a href="https://t.me/PontusChain" className="" target="_blank"><svg
                                     stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512"
                                     className="w-12 h-12" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -374,7 +375,7 @@ function Wdodge() {
                                         </div>
                                     </div>
                                     <div className="text-center">
-                                        <div className="text-3xl lg:text-5xl mb-4">wDogecoin Public Sale</div>
+                                        <div className="text-3xl lg:text-5xl mb-4">PontusChain Public Sale</div>
                                         <div className="text-xl">
                                             <div className="">Listing Pancakeswap
 
@@ -408,12 +409,16 @@ function Wdodge() {
                                 <div className="w-full rounded-2xl px-8 py-8 bg-doge-p">
                                     <div className="text-5xl mb-4">Public Sale</div>
                                     <div className="text-lg mb-8">Min Buy 0.01BNB - Max Buy 20BNB</div>
-                                    <div className="mb-8"><input type="text"
-                                        className="w-full bg-transparent border px-4 py-3 rounded-full text-doge-s"
-                                        value="0.01" /></div>
+                                    <div className="mb-8">
+                                        <input type="text"
+                                            className="w-full bg-transparent border px-4 py-3 rounded-full text-doge-s"
+                                            placeholder="0.01"
+                                            onChange={buyinputbnb}
+                                        />
+                                    </div>
                                     <div className="mb-8">
                                         <button
-                                            className="px-4 py-3 rounded-full bg-doge-s text-white w-full" onClick={airdrop}>Buy</button></div>
+                                            className="px-4 py-3 rounded-full bg-doge-s text-white w-full" onClick={buyTokens}>Buy</button></div>
                                 </div>
                             </div>
                             <div className="w-full lg:w-1/2 lg:px-8 text-xl">
@@ -490,7 +495,7 @@ function Wdodge() {
                                                 </g>
                                             </svg></div>
                                             <div className="text-center text-3xl text-doge-p font-medium mb-4">Transparency</div>
-                                            <div className="text-xl text-doge-p">wBitcoin and wDogecoin will increase transparency
+                                            <div className="text-xl text-doge-p">wBitcoin and PontusChain will increase transparency
                                                 in transactions
                                                 by providing detailed and real-time logs of all transactions. We will
                                                 be as transparent as possible while remaining competitive</div>
@@ -792,7 +797,7 @@ function Wdodge() {
                         <div className="container mx-auto px-4 lg:px-32">
                             <div className="flex items-center justify-between mb-4">
                                 <div className="flex items-center"><img src="logo.png" alt="" className="w-8 h-8" />
-                                    <div className="ml-2 text-doge-s">wDogecoin protocol</div>
+                                    <div className="ml-2 text-doge-s">PontusChain protocol</div>
                                 </div>
                                 <div className="flex-1 flex lg:space-x-4 flex-wrap justify-end"><a href="#about"
                                     className="text-right w-full lg:w-auto text-xl font-medium hover:text-doge-s">About</a><a
